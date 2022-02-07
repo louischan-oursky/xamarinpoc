e98b866123b9688e25ad2a9abc1d05bc79d9eb40
Louis Chan <louischan@oursky.com>
2022-02-07 18:06:07 +0800

    Add AuthgearIOS

---
+++ authgeariosbinding/authgeariosbinding/AuthgearIOS.cs
@@ -0,0 +1,16 @@
+﻿using authgear;
+
+namespace authgeariosbinding
+{
+    public class AuthgearIOS: Authgear
+    {
+        public AuthgearIOS()
+        {
+        }
+
+        public void SayHello()
+        {
+            System.Console.WriteLine("Hello from iOS");
+        }
+    }
+}
