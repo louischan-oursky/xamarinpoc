be90b9c5647ed8fbefc2430e9fce61e2698828cf
Louis Chan <louischan@oursky.com>
2022-02-08 19:27:56 +0800

    Add swift framework to iOS binding library

--- authgeariosbinding/authgeariosbinding/ApiDefinition.cs
+++ authgeariosbinding/authgeariosbinding/ApiDefinition.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using Foundation;
+
+namespace authgeariosbinding
+{
+	// @interface AuthgearSwift : NSObject
+	[BaseType(typeof(NSObject), Name = "_TtC13authgearswift13AuthgearSwift")]
+	interface AuthgearSwift
+	{
+		// -(void)greet:(NSString * _Nonnull)name completion:(void (^ _Nonnull)(NSString * _Nonnull))completion;
+		[Export("greet:completion:")]
+		void Greet(string name, Action<NSString> completion);
+	}
+}
