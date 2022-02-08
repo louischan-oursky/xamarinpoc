e58ae6059ac7cf055e9dc0a869af5559b471d4ed
Louis Chan <louischan@oursky.com>
2022-02-08 16:49:40 +0800

    Generate Swift framework library

---
+++ authgearswift/authgearswift/authgearswift.h
@@ -0,0 +1,18 @@
+//
+//  authgearswift.h
+//  authgearswift
+//
+//  Created by louischan on 8/2/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for authgearswift.
+FOUNDATION_EXPORT double authgearswiftVersionNumber;
+
+//! Project version string for authgearswift.
+FOUNDATION_EXPORT const unsigned char authgearswiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <authgearswift/PublicHeader.h>
+
+
