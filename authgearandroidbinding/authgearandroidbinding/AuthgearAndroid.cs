8f75684918dd64d61bb76ac8b01b6d76c46b16a4
Louis Chan <louischan@oursky.com>
2022-02-09 13:21:46 +0800

    Implement Authgear with AuthgearKotlin

--- authgearandroidbinding/authgearandroidbinding/AuthgearAndroid.cs
+++ authgearandroidbinding/authgearandroidbinding/AuthgearAndroid.cs
@@ -1,16 +1,41 @@
-﻿using authgear;
+﻿using System;
+using authgear;
+using Com.Iawaknahc.Xamarin.Kotlin;
 
 namespace authgearandroidbinding
 {
 	public class AuthgearAndroid: Authgear
 	{
+        private AuthgearKotlin authgearKotlin;
+
+
 		public AuthgearAndroid()
 		{
+            this.authgearKotlin = new AuthgearKotlin();
 		}
 
-        public void SayHello()
+        public void SayHello(Action<string> completion)
         {
-            System.Console.WriteLine("Hello from Android");
+            this.authgearKotlin.Greet("AndroidKotlin", new Completion(delegate (Java.Lang.Object result, Java.Lang.Throwable error)
+            {
+                Java.Lang.String jString = (Java.Lang.String) result;
+                completion.Invoke(result.ToString());
+            }));
+        }
+    }
+
+    public class Completion: Java.Lang.Object, ICompletion
+    {
+        private Action<Java.Lang.Object, Java.Lang.Throwable> action;
+
+        public Completion(Action<Java.Lang.Object, Java.Lang.Throwable> action)
+        {
+            this.action = action;
+        }
+
+        public void Invoke(Java.Lang.Object result, Java.Lang.Throwable error)
+        {
+            this.action.Invoke(result, error);
         }
     }
 }
